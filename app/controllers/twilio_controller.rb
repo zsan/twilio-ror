274998968a43c10a547972ed32043340f97b404b
zaksan <zaksantanu@gmail.com>
2017-09-09 21:43:03 +0700

    it seems i used old tutorial, fixed

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -1,11 +1,13 @@
+require 'twilio-ruby'
+
 class TwilioController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def voice
-    response = Twilio::TwiML::Response.new do |r|
-      r.Say "Yay! You're on Rails!", voice: "alice"
-      r.Sms "Well done building your first Twilio on Rails 5 app!"
-      r.Play "http://linode.rabasa.com/cantina.mp3"
+    response = Twilio::TwiML::VoiceResponse.new do |r|
+      r.say "Yay! You're on Rails!", voice: "alice"
+      # r.sms "Well done building your first Twilio on Rails 5 app!"
+      # r.play "http://linode.rabasa.com/cantina.mp3"
     end
     render :xml => response.to_xml
   end
